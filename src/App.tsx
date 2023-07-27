dfd44c827690c9dcaee891193a6d0b4143e2fbc4
Pedro Lima <tsc.pedrolima@gmail.com>
2023-07-27 17:46:44 -0300

    feat: add home route

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.scss";
-import Root from "./components/root";
+import Root from "./components/Root";
+import Home from "./routes/Home";
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter(createRoutesFromElements(
 	<Route path="/" element={ <Root /> }>
+		<Route path="/" element={ <Home /> } />
 	</Route>
 ));
 
--- src/components/root/index.tsx
+++ src/components/root/index.tsx
@@ -1,9 +1,9 @@
-
+import { Outlet } from "react-router-dom";
 
 function Root() {
 	return (
 		<div>
-			<h1>Root</h1>
+			<Outlet />
 		</div>
 	);
 }
