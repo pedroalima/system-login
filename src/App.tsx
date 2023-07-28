1dd2e4999ac8219e5c0df27d5ae0a5ed543a4a56
Pedro Lima <tsc.pedrolima@gmail.com>
2023-07-28 15:39:22 -0300

    feat: add signup route and types file

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import "./App.scss";
 
+import { PrivateProps } from "./types";
+
 import Root from "./components/Root";
 import SignIn from "./routes/SignIn";
-// import Home from "./routes/Home";
+import Home from "./routes/Home";
 
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
 
-// const Private = () => {
-// 	const signed = false;
+const Private = ({ Item }: PrivateProps) => {
+	const signed = false;
 
-// 	return signed ? <Item /> : <SignIn />;
-// };
+	return signed ? <Item /> : <SignIn />;
+};
 
 const router = createBrowserRouter(createRoutesFromElements(
 	<Route path="/" element={ <Root /> }>
 		<Route path="/" element={ <SignIn /> } />
-		{/* <Route path="/home" element={ < />} /> */}
+		<Route path="/home" element={ <Private Item={Home} />} />
 	</Route>
 ));
 
